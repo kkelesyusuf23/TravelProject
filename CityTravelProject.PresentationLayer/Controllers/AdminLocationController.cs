44c54e5d69e1dc7d72cc9200a43776833f695112
kkelesyusuf <kkelesyusuf23@gmail.com>
2024-05-28 02:34:48 +0300

    Admin Layout oluşturuldu.

---
+++ CityTravelProject.PresentationLayer/Controllers/AdminLocationController.cs
@@ -0,0 +1,85 @@
+﻿using CityTravelProject.DtoLayer.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CityTravelProject.PresentationLayer.Controllers
+{
+    public class AdminLocationController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminLocationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        //public async Task<IActionResult> Index()
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var responseMessage = await client.GetAsync("https://localhost:7188/api/Location");
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+        //        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        //        var values = JsonConvert.DeserializeObject<List<ResultLocation>>(jsonData);
+        //        return View(values);
+        //    }
+        //    return View();
+        //}
+        //[HttpGet]
+        //public IActionResult CreateAbout()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> CreateAbout(CreateAboutDto createAboutDto)
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var jsonData = JsonConvert.SerializeObject(createAboutDto);
+        //    StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        //    var responseMessage = await client.PostAsync("https://localhost:7272/api/Abouts", content);
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
+
+        //public async Task<IActionResult> DeleteAbout(int id)
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var responseMessage = await client.DeleteAsync("https://localhost:7272/api/Abouts?id=" + id);
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
+        //[HttpGet]
+        //public async Task<IActionResult> UpdateAbout(int id)
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var responseMessage = await client.GetAsync("https://localhost:7272/api/Abouts/" + id);
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+        //        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        //        var values = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+        //        return View(values);
+        //    }
+        //    return View();
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAboutDto)
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var jsonData = JsonConvert.SerializeObject(updateAboutDto);
+        //    StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        //    var responseMessage = await client.PutAsync("https://localhost:7272/api/Abouts/", content);
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
+    }
+}
