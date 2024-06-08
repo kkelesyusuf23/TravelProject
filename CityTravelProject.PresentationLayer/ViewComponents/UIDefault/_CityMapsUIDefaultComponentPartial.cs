45bab55b78e389f7f8d932ab41bc522d272768b9
kkelesyusuf <kkelesyusuf23@gmail.com>
2024-06-08 22:26:01 +0300

    ...

--- CityTravelProject.PresentationLayer/ViewComponents/UIDefault/_CityMapsUIDefaultComponentPartial.cs
+++ CityTravelProject.PresentationLayer/ViewComponents/UIDefault/_CityMapsUIDefaultComponentPartial.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityTravelProject.PresentationLayer.Dtos.CityMapsDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CityTravelProject.PresentationLayer.ViewComponents.UIDefault
 {
     public class _CityMapsUIDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CityMapsUIDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7188/api/CityMaps");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCityMapsDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
+
+
+        
     }
 }
--- CityTravelProject.PresentationLayer/ViewComponents/UIDefault/_SliderUIDefaultComponentPartial.cs
+++ CityTravelProject.PresentationLayer/ViewComponents/UIDefault/_SliderUIDefaultComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityTravelProject.PresentationLayer.Dtos.FutureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CityTravelProject.PresentationLayer.ViewComponents.UIDefault
 {
     public class _SliderUIDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _SliderUIDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7188/api/Future");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFutureDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
