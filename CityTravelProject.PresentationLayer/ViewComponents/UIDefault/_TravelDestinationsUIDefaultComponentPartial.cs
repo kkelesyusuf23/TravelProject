d4e7942a3bb4ebca97b80a5d1f3eb2d66d01f9a5
kkelesyusuf <kkelesyusuf23@gmail.com>
2024-06-09 11:32:41 +0300

    ...

--- CityTravelProject.PresentationLayer/ViewComponents/UIDefault/_TravelDestinationsUIDefaultComponentPartial.cs
+++ CityTravelProject.PresentationLayer/ViewComponents/UIDefault/_TravelDestinationsUIDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityTravelProject.PresentationLayer.Dtos.TravelDestinationsDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CityTravelProject.PresentationLayer.ViewComponents.UIDefault
 {
     public class _TravelDestinationsUIDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TravelDestinationsUIDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7188/api/TravelDestinations");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTravelDestinationsDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
