7abd714bfd021804feb135261f0469fd6d6aad39
kkelesyusuf <kkelesyusuf23@gmail.com>
2024-06-18 13:09:07 +0300

    ....

--- CityTravelProject.PresentationLayer/Controllers/UIContactController.cs
+++ CityTravelProject.PresentationLayer/Controllers/UIContactController.cs
@@ -1,11 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityTravelProject.PresentationLayer.Dtos.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace CityTravelProject.PresentationLayer.Controllers
 {
 	public class UIContactController : Controller
 	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public UIContactController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
 		public IActionResult Index()
 		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Index(CreateContactDto createContactDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createContactDto);
+			StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7188/api/Contact", content);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
 			return View();
 		}
 	}
